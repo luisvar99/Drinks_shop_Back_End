674a3257eed46c05a880b78f47178b88c07b779e
luisvar99 <luisvar2703@gmail.com>
2022-06-02 15:03:48 -0400

    Vercel Config

--- db.js
+++ db.js
@@ -3,10 +3,8 @@ const {dbConfig} = require('./config');
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
-
 const db = new Pool ({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     }, 
