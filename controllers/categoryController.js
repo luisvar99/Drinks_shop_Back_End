7a65c8d120a74709ee1a3fe0e6202a083abb3c01
luisvar99 <luisvar2703@gmail.com>
2022-06-01 20:07:31 -0400

    probando

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,6 +2,7 @@ const {db} = require('../db');
 
 const getAllCategories = async (req, res, next) => {
     try {
+        res.json({getCategories: "Get categories"})
         const allCategories = await db.query('SELECT * FROM categories')
         return res.json(allCategories.rows);
     } catch (error) {
