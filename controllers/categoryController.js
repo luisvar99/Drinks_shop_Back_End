fc49135b5cc28bce40fe0a0361c57e78433dea00
luisvar99 <luisvar2703@gmail.com>
2022-06-01 21:09:37 -0400

    a

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,8 +2,8 @@ const {db} = require('../db');
 
 const getAllCategories = async (req, res, next) => {
     try {
-        res.json({getCategories: "Entrando a Get categories"})
-        res.json({Query: "After Query"})
+        //res.json({getCategories: "Entrando a Get categories"})
+        //res.json({Query: "After Query"})
         const allCategories = await db.query('SELECT * FROM categories')
         res.json(allCategories.rows);
     } catch (error) {
