947ae63c72e5b36aef6ee7ecd37e2ae73211d2a5
luisvar99 <luisvar2703@gmail.com>
2022-06-02 16:05:06 -0400

    Heroku Config

--- db.js
+++ db.js
@@ -6,7 +6,6 @@ const db = new Pool ({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.PORT,
     
 /*     ssl: {
         rejectUnauthorized: false,
