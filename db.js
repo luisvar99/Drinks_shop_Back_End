a748a62312c91ce59434dcc6aeefbab37fb8939b
luisvar99 <luisvar2703@gmail.com>
2022-06-01 22:26:05 -0400

    a

--- db.js
+++ db.js
@@ -3,9 +3,7 @@ const {dbConfig} = require('./config');
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.USER}:
-${process.env.PASSWORD}@${process.env.HOST}:
-${process.env.PORT}/${process.env.DATABASE}`;
+const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
 
 const db = new Pool ({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
